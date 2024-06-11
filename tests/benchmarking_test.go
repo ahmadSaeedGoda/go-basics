1b26a29a0da04021b6d9f4dc9ad55e8aee57fafd
ahmadsaeedgoda <ahmad.saeed.goda@gmail.com>
2024-06-11 11:32:32 +0300

    Add explanation & instruction for benchmarking
    - Write comments to explain benchmarking test at `tests/benchmarking_test.go` for clarity and instructions of usage.

--- tests/benchmarking_test.go
+++ tests/benchmarking_test.go
@@ -31,3 +31,27 @@ func BenchmarkFibIteratively(b *testing.B) {
 		_ = Fib(20, false)
 	}
 }
+
+/*
+	Run the following command, if your IDE doesn't help running in GUI, for testing with benchmarking in CLI:
+	bash
+	```
+	$ go test -bench=. ./tests/
+	```
+
+	Expected output result should be similar to the following:
+	```
+	$ go test -bench=. ./tests/
+		goos: linux
+		goarch: amd64
+		pkg: examples/tests
+		cpu: 12th Gen Intel(R) Core(TM) i5-12400
+		BenchmarkFibRecursively-12         37012             31629 ns/op
+		BenchmarkFibIteratively-12      160066993                7.380 ns/op
+		PASS
+		ok      examples/tests  3.442s
+	```
+	As you could see, the difference between iterative vs recursive approach, performance wise
+	and time complexity wise, is way remarkable!
+	Let alone the space complexity.
+*/
